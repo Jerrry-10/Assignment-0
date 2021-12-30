55f9f8b8512196b0e2c68a8f06af831689d39fae
Jerry <Jerry.Aviles@cix.csi.cuny.edu>
2021-12-30 00:02:08 -0500

    5th commit

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,11 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let counter = 0;
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] === true ||  arr[i] === false){
+      counter++;
+    }
+  }
+  return counter;
 }
 
 // Do not edit this line;
