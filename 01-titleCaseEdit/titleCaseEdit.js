8861bb31e4cb4b67320a243bf303e77bbff29123
Jerry <Jerry.Aviles@cix.csi.cuny.edu>
2021-12-29 23:08:21 -0500

    3rd commit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,9 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let lower = title.toLowerCase().split(' ')
+  for(var i = 0; i < lower.length; i++ ){
+    lower[i] = lower[i].replace(lower[i].charAt(0),lower[i].charAt(0).toUpperCase());
+  }
+  return lower.join(' ');
 }
 
 // Do not edit this line;
